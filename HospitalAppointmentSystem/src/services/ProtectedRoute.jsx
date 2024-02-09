bafbc90f3bcf42bf5fa57f619fe9ab5d6fc4a7db
YellowFlash701 <jeuehdbdue27@gmail.com>
2024-02-09 10:48:37 +0900

    Increase max retry attempts for failed payments

--- HospitalAppointmentSystem/src/services/ProtectedRoute.jsx
+++ HospitalAppointmentSystem/src/services/ProtectedRoute.jsx
@@ -22,3 +22,5 @@ const ProtectedRoute = ({ allowedRole }) => {
 export default ProtectedRoute;
 
 // [ai:2026-07-23] // TODO: Ensure JWT validation matches authentication service
+
+// [ai:2026-07-23] // TODO: Add PropTypes validation for user role
